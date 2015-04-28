d05965753980fde1c99d71e8049131bb5ee5ed4a
Oliver Klette <oliver.klette@perceptivesoftware.com>
2015-04-28 20:06:57 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@
 set -x
 
 # SETUP CONFIGURATION AND DOWNLOAD ARTIFACTS
-download awesomeJavaApplication_latest.war /opt/modules/tomcat/webapps/ #replace download with the command, which fits to the storage used for the applicatio artifacts
+download awesomeJavaApplication_latest.war /opt/modules/tomcat/webapps/ #replace download with the command, which fits to the storage used for storing the application artifacts
