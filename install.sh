991ef8eb4a61f1226a4160931ee53e71635568c0
Oliver Klette <oliver.klette@perceptivesoftware.com>
2015-04-29 09:53:47 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
+# Script will error out if any command fails, thus failing the build
+set -e
+
+# Prints each command before it runs.
 set -x
 
 # SETUP CONFIGURATION AND DOWNLOAD ARTIFACTS
-download awesomeJavaApplication_latest.war /opt/modules/tomcat/webapps/ #replace download with the command, which fits to the storage used for storing the application artifacts
+cp /build/config.xml /opt/modules/tomcat/conf/ 
+
+download awesomeJavaApplication-$1.war /opt/modules/tomcat/webapps/ #replace download with the command, which fits to the storage used for storing the application artifacts
